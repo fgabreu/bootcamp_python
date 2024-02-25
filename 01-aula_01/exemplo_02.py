65935a88e810b8d4f4154e87caa38454ee17014d
˜Felipe˜ <eng.felipeabreu@gmail.com>
2024-02-24 21:11:27 -0300

    adicionar o exemplo 02

---
+++ 01-aula_01/exemplo_02.py
@@ -0,0 +1,6 @@
+# Criar um programa onde o usuário digite dois valores e apareca a soma
+
+v1 = float(input("Digite o primeiro valor: "))
+v2 = float(input("Digite o segundo valor: "))
+
+print(v1+v2)
